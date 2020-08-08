c460b5f2c93b6f52ba0f91a2c50b105ab9bd3ccf
Mohamed Haneef <mhaneef50673@gmail.com>
2020-08-08 20:13:58 +0400

    Add: endpoint changed to heruko

--- src/core/api/endpoints.js
+++ src/core/api/endpoints.js
@@ -1,4 +1,4 @@
-export const serviceUrl = 'http://localhost:3001/api/v1/';
+export const serviceUrl = 'https://haneef-tic-tac-toe-api.herokuapp.com/api/v1/';
 
 export const getLeaderboardUrl = `${serviceUrl}getLeaderboard`;
 export const saveLeaderboardUrl = `${serviceUrl}saveLeaderboard`;
