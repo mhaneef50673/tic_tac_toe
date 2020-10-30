a7fcbc3b295d6ec11d8c0b527932cd5accf391b8
Mohamed  Haneef <mohamed.haneef@basharq.com>
2020-10-30 21:41:06 +0400

    added an iframe

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -5,7 +5,28 @@ import TicTacToe from "../../organisms/tic-tac-toe";
  * Renders when any url doesnt match the route
  */
 const Home = (props) => {
-  return <TicTacToe />;
+  return (
+    <>
+      <div
+        style={{
+          padding: "0 15px",
+        }}
+      >
+        <iframe
+          title="test ifrmae"
+          src={
+            "https://graphics.reuters.com/USA-ELECTION/RESULTS-LIVE-US/gjnpwlbyyvw/media-embed.html?headline=false"
+          }
+          width="100%"
+          height={"3350px"}
+          style={{
+            border: "none",
+          }}
+        />
+      </div>
+      <TicTacToe />
+    </>
+  );
 };
 
 export default Home;
